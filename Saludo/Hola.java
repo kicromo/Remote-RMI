a63b0ebe4f7b56580c5c16f0ddcba6658f19e350
kicromo <128406055+kicromo@users.noreply.github.com>
2024-10-17 12:19:33 +0200

    Delete Saludo directory

--- Saludo/Hola.java
+++
@@ -1,8 +0,0 @@
-package Saludo;
-
-// Ejemplo de interfaz remota
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-public interface Hola extends Remote {
-    String di_hola() throws RemoteException;
-}
